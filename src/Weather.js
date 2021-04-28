de96c4190bc679d87138feb9f2da561dea77c57e
AnjaRL <AnjaRL@users.noreply.github.com>
2021-04-28 18:44:22 +0400

    Loader and color

--- src/Weather.js
+++ src/Weather.js
@@ -16,8 +16,8 @@ export default function Weather(props) {
 
   return (
     <Loader
-      type="ThreeDots"
-      color="white"
+      type="Puff"
+      color="#00BFFF"
       height={100}
       width={100}
       timeout={3000}
